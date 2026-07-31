1754f13f842f8fee17816c0ea3ff8dd0312d2e61
durandadvo-ops <durandadvo@gmail.com>
2026-07-31 17:32:00 -0300

    fix: usar nova foto JPEG na localizacao

--- client/src/components/LocationMap.tsx
+++ client/src/components/LocationMap.tsx
@@ -14,7 +14,7 @@ export default function LocationMap() {
       <div className="location-card">
         <div className="location-card__image-wrap">
           <img
-            src="/assets/escritorio-localizacao-v3.webp"
+            src="/assets/escritorio-localizacao-v4.jpg"
             alt="Fachada do escritório Eufrauzino Advogados"
             className="location-card__image"
           />
