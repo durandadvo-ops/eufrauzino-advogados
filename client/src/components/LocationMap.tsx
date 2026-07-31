31c43ff9af8d2f3e5257f244605b37c34f92ebf9
durandadvo-ops <durandadvo@gmail.com>
2026-07-31 18:12:52 -0300

    fix: usar ativo textual valido na localizacao

--- client/src/components/LocationMap.tsx
+++ client/src/components/LocationMap.tsx
@@ -14,7 +14,7 @@ export default function LocationMap() {
       <div className="location-card">
         <div className="location-card__image-wrap">
           <img
-            src="/assets/escritorio-localizacao-v4.jpg"
+            src="/assets/escritorio-localizacao-final.svg"
             alt="Fachada do escritório Eufrauzino Advogados"
             className="location-card__image"
           />
